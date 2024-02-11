4f6ff58ea4a8f1f72d957df946cf676829bdea2d
Geoff Kimball <geoff@geoffkimball.com>
2024-02-11 15:28:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# bf-wasm
+
+Two implementations of Brainfuck, one in JavaScript and one in AssemblyScript, compiled to WASM. Just messing around with AssemblyScript and doing some basic benchmarking. (The JS implementation is faster.)
+
+```
+git clone https://github.com/gakimball/bf-wasm
+cd bf-wasm
+npm i
+npm run asbuild
+npm start
+```
+
+## License
+
+MIT &copy; [Geoff Kimball](https://geoffkimball.com)
---
+++ assembly/index.ts
@@ -0,0 +1,74 @@
+export function execute(input: i8[]): i8[] {
+  let pc = 0
+  let dc = 0
+  let oc = 0
+  const data = new Array<i8>(100)
+  const output = new Array<i8>(100)
+
+  data.fill(0)
+  output.fill(0)
+
+  while (pc < input.length) {
+    switch (input[pc]) {
+      case 0: // >
+        dc++
+        break
+      case 1: // <
+        dc--
+        break
+      case 2: // +
+        data[dc] += 1
+        break
+      case 3: // -
+        data[dc] -= 1
+        break
+      case 4: // .
+        output[oc++] = data[dc]
+        break
+      case 5: // ,
+        break
+      case 6: { // [
+        if (data[dc] === 0) {
+          let brackets = 0
+
+          while (brackets > -1) {
+            const token = input[++pc]
+
+            if (token === 6) { // [
+              brackets++
+            } else if (token === 7) { // ]
+              brackets--
+            }
+          }
+        }
+        break
+      }
+      case 7: { // ]
+        if (data[dc] !== 0) {
+          let brackets = 0
+
+          while (brackets > -1) {
+            const token = input[--pc]
+
+            if (token === 7) { // ]
+              brackets++
+            } else if (token === 6) { // [
+              brackets--
+            }
+          }
+        }
+        break
+      }
+    }
+
+    pc++
+  }
+
+  return output
+}
+
+export function executeMulti(input: i8[]): void {
+  for (let i = 0; i < 1000; i++) {
+    execute(input)
+  }
+}
